4ec5ee157d2873712c8be23b116ab5aa4c3becee
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-04-20 10:24:37 +0300

    setup.bat install pip

--- setup.bat
+++ setup.bat
@@ -3,9 +3,13 @@
 cls
 echo 'Installing dependencies in 3 seconds...'
 timeout /t 3
+echo 'Installing pip...'
+python get-pip.py
+timeout /t 2
+echo 'Installing modules...'
 pip install -r requirements.txt
 timeout /t 1
-set /p input= y/n
+set /p input= Start H4XTools now? (y/n)
 if %input%==y python h4xtools.py
 else echo 'Exiting...'
 timeout /t 1
