243daaa27c818e581730eba69250f7b35e144e63
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-04-20 10:30:51 +0300

    setup.bat

--- setup.bat
+++ setup.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 :start
 cls
-echo 'Installing dependencies in 3 seconds...'
+echo Installing dependencies in 3 seconds...
 timeout /t 3
-echo 'Installing pip...'
-python get-pip.py
+echo Installing pip...
+python -m pip install --upgrade pip
 timeout /t 2
-echo 'Installing modules...'
+echo Installing modules...
 pip install -r requirements.txt
 timeout /t 1
 set /p input= Start H4XTools now? (y/n)
 if %input%==y python h4xtools.py
-else echo 'Exiting...'
+else echo Exiting...
 timeout /t 1
 :exit
