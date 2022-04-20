7d68e18e8d1cac1785675d93767477107da3b472
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-04-20 10:38:20 +0300

    nobreak

--- setup.bat
+++ setup.bat
@@ -2,11 +2,11 @@
 :start
 cls
 echo Installing dependencies in 3 seconds...
-timeout /t 3
+timeout /t 3 /nobreak
 pip3 install -r requirements.txt
-timeout /t 1
+timeout /t 1 /nobreak
 set /p input= Start H4XTools now? (y/n)
 if %input%==y python h4xtools.py
 else echo Exiting...
-timeout /t 1
+timeout /t 1 /nobreak
 :exit
