46ed326482d1507efdde3d6004b641f816220233
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-04-27 15:45:42 +0300

    igdox fix

--- utils/igdox.py
+++ utils/igdox.py
@@ -5,7 +5,7 @@ from colorama import Fore
 
 class dox:
     def __init__(self, username):
-        link = f"https://www.instagram.com/{username}/channel/?__a=1"
+        link = f"https://www.instagram.com/{username}/?__a=1"
         session = requests.session()
         session.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'}
         response = session.get(link)
