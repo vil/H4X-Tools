e963a75ad1870dafef560899423e2411148ed4e0
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-04-27 10:29:56 +0300

    webhook avatar url

--- utils/webhook_spammer.py
+++ utils/webhook_spammer.py
@@ -6,7 +6,8 @@ class spam:
     def __init__(self, url, amount, message):
         data = {
         "content" : message,
-        "username" : "H4X-Tools" }
+        "username" : "H4X-Tools",
+        "avatar_url" : "https://cdn.discordapp.com/attachments/817858188753240104/821111284962689125/7ab097df97e8b8b41dd177a073867824_400x400.jpeg" }
 
         try:
             for i in range(1, amount + 1):
