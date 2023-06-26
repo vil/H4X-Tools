9ccc4c0dac8db9647aa42d3777862ddc70e60c26
vili <me@vili.dev>
2023-06-26 10:18:47 +0300

    Revert f3f05fb4 as bad design

--- update.sh
+++ update.sh
@@ -1,44 +1,28 @@
 #!/usr/bin/env sh
 
 clear
-cat << "EOF"
+echo """
     //    / /        \\ / /      /__  ___/ //   ) ) //   ) ) / /        //   ) )
    //___ / //___/ /   \  /         / /    //   / / //   / / / /        ((
   / ___   /____  /    / /   ____  / /    //   / / //   / / / /           \\
  //    / /    / /    / /\\       / /    //   / / //   / / / /              ) )
 //    / /    / /    / /  \\     / /    ((___/ / ((___/ / / /____/ / ((___ / /
 
-~~by Vili (https://github.com/v1li)
+by Vili (https://github.com/v1li)
 
-EOF
-
+"""
 echo
 echo
-echo "Make sure to run this in the main directory if this script cant find it automatically."
+echo Make sure to run this in the main directory!
 echo
-echo "Checking for H4X-Tools directory location..."
-HOME_DIR=$(eval echo "~")
-H4XTOOLS_FILE="$HOME_DIR/.h4xtools"
-
-sleep 1
-
-if [ -f "$H4XTOOLS_FILE" ]; then
-    source "$H4XTOOLS_FILE"
-    echo "H4X-Tools directory location found: $H4XTOOLS_DIR"
-    echo
-    echo "Do you want to update H4XTools? [y/n]"
-    read -r answer
-    if [ "$answer" = "y" ]; then
-        cd "$H4XTOOLS_DIR" || exit
-        git fetch
-        git pull
-    fi
-else
-    echo "H4X-Tools directory location not found. Update failed."
+echo Do you want to update H4XTools? [y/n]
+read answer
+if [ "$answer" = "y" ]; then
+    git fetch
+    git pull
 fi
-
-echo "Run setup.sh to apply changes. Do it now? [y/n]"
-read -r answer
+echo Run setup.sh to apply changes. Do it now? [y/n]
+read answer
 if [ "$answer" = "y" ]; then
     sh setup.sh
 fi
