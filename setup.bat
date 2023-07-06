8570a593b91fcdd3915149cf310cdf68dadb58c2
vili <me@vili.dev>
2023-07-06 20:58:25 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -17,15 +17,16 @@ timeout /t 1 /nobreak > nul
 echo.
 echo Done..!
 echo.
-echo Building H4XTools to a single executable in 3 seconds...
+echo Building H4X-Tools to a single executable in 3 seconds...
 timeout /t 3 /nobreak > nul
 python -m PyInstaller --onefile h4xtools.py
 echo.
 echo Done..!
-echo Your H4XTools executable is located in the dist folder. You can now move it to your desired location.
+echo Your H4X-Tools executable is located in the 'dist' folder. You can now move it to your desired location.
 echo.
+echo OR you can start H4X-Tools with python by typing 'python h4xtools.py' in the terminal.
 
-set /p "input=Start H4XTools now? (y/n) -> "
+set /p "input=Start H4X-Tools now? (y/n) -> "
 if /i "%input%"=="y" (
     echo Starting H4XTools...
     timeout /t 1 /nobreak > nul
