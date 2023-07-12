892e2b19f1a336adb7770e8067ed04489979033c
vili <me@vili.dev>
2023-07-12 16:47:37 +0300

    Update phonenumber loookup

--- utils/phonenumber_lookup.py
+++ utils/phonenumber_lookup.py
@@ -16,8 +16,7 @@
  """
 
 import phonenumbers as p
-from phonenumbers import geocoder
-from phonenumbers import carrier
+from phonenumbers import carrier, geocoder, timezone
 from helper import printer
 import time
 
@@ -32,17 +31,22 @@ class LookUp:
         print("\n")
         try:
             ph_no = p.parse(no)
-            geo_location = geocoder.description_for_number(ph_no, 'en')
-            no_carrier = carrier.name_for_number(ph_no, 'en')
+            country = p.region_code_for_country_code(ph_no.country_code)
+            no_carrier = carrier.name_for_number(ph_no, "en")
             no_valid = p.is_valid_number(ph_no)
             no_possible = p.is_possible_number(ph_no)
+            time_zone = timezone.time_zones_for_number(ph_no)
+            region = geocoder.description_for_number(ph_no, "en")
+
             printer.info(f"Trying to find the information of '{no}'")
             time.sleep(1)
+            printer.success("Phone Number -", no)
             printer.success(f"Valid Number -", no_valid)
             printer.success(f"Possible Number -", no_possible)
-            printer.success(f"Country -", geo_location)
             printer.success(f"Sim Provider -", no_carrier)
-            printer.success("\n")
+            printer.success(f"Country -", country)
+            printer.success(f"Region -", region)
+            printer.success(f"Time Zone -", time_zone)
         except Exception as e:
             printer.error(f"Error : ", e)
             pass
