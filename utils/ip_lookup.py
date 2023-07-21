761b746cc6ece7a9e049c362fe22b4aa9039812c
vili <me@vili.dev>
2023-07-21 16:37:47 +0300

    Updated iplookup

--- utils/ip_lookup.py
+++ utils/ip_lookup.py
@@ -17,35 +17,42 @@
 
 import json
 import time
-from urllib.request import urlopen
+import requests
+import random
+import socket
 from helper import printer
+from utils import randomuser
 
 
 class Lookup:
     """
-    Gets information about a given ip address using ip-api.com.
+    Gets information about a given ip address using https://ipinfo.io/
 
     :param ip: The ip address to search for.
     """
     def __init__(self, ip):
         try:
-            url = "http://ip-api.com/json/" + ip
-            values = json.load(urlopen(url))
+            ip = socket.gethostbyname(ip)
+            url = f"https://ipinfo.io/{ip}/json"
+            headers = {'User-Agent': random.choice(randomuser.users)}
+            url = requests.get(url, headers=headers)
+            # printer.info(url.text)
+            values = json.loads(url.text)
 
-            printer.info(f"Trying to find information for '{ip}'")
+            printer.info(f"Trying to find information for '{ip}'...")
             time.sleep(1)
-            printer.success(f"Ip Address - ", values['query'])
-            printer.success(f"Country - ", values['country'])
-            printer.success(f"City - ", values['city'])
-            printer.success(f"ISP - ", values['isp'])
-            printer.success(f"Region - ", values['regionName'])
-            printer.success(f"Timezone - ", values['timezone'])
-            printer.success(f"Zip - ", values['zip'])
-            printer.success(f"Lat - ", values['lat'])
-            printer.success(f"Lon - ", values['lon'])
-            printer.success(f"AS - ", values['as'])
-            printer.success(f"Maps URL - ", f"https://www.google.com/maps/search/{values['lat']},{values['lon']}")
+
+            for value in values:
+                # If value contains readme, skip it.
+                if value == "readme":
+                    continue
+                elif value == "" or value is None:
+                    value = "Not Found"
+
+                printer.success(f"{value.capitalize()} - ", values[value])
+
+            printer.success(f"Maps URL - ", f"https://www.google.com/maps/search/{values['loc']}")
 
         except Exception as e:
-            printer.error(f"Error : ", e)
+            printer.error(f"Error : {e}")
             pass
