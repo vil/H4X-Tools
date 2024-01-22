af859bb5b4fd42b7ed9beb48e84496430d04b887
Vili <vili@riseup.net>
2024-01-22 16:31:43 +0200

    Improved basexx.py code

--- utils/basexx.py
+++ utils/basexx.py
@@ -24,45 +24,42 @@ class BaseXX:
     Encodes or decodes a text using the Base64/32/16 algorithm.
 
     :param message: The message to encode or decode.
-    :param mode: The mode to use for the encoding or decoding ('encrypt' or 'decrypt').
-    :param encoding: The encoding to use for the encoding or decoding ('base64' or 'base32' or 'base16').
+    :param mode: The mode to use for the encoding or decoding ('encode' or 'decode').
+    :param encoding: The encoding to use for the encoding or decoding ('64' or '32' or '16').
     """
     def __init__(self, message, mode, encoding):
         self.message = message
         self.mode = mode
         self.encoding = encoding
 
-        if self.mode in ("encode", "e"):
-            printer.info(f"Encoding '{self.message}' into Base{encoding}...")
+        if self.mode == "encode":
+            printer.info(f"Encoding '{self.message}' into Base{self.encoding}...")
             self.encode()
-        elif self.mode in ("decode", "d"):
-            printer.info(f"Decoding '{self.message}' from Base{encoding}...")
+        elif self.mode == "decode":
+            printer.info(f"Decoding '{self.message}' from Base{self.encoding}...")
             self.decode()
         else:
-            printer.error("Invalid mode, please choose either 'encode' or 'decrypt'..!")
+            printer.error("Invalid mode, please choose either 'encode' or 'decode'..!")
 
     def encode(self):
         try:
-            if self.encoding == "64":
-                self.encoded_message = base64.b64encode(self.message.encode("ascii")).decode("ascii")
-            elif self.encoding == "32":
-                self.encoded_message = base64.b32encode(self.message.encode("ascii")).decode("ascii")
-            elif self.encoding == "16":
-                self.encoded_message = base64.b16encode(self.message.encode("ascii")).decode("ascii")
+            if self.encoding in ("64", "32", "16"):
+                encoding_method = getattr(base64, f'b{self.encoding}encode')
+                self.encoded_message = encoding_method(self.message.encode("ascii")).decode("ascii")
+                printer.info(f"'{self.message}' in Base{self.encoding} : {self.encoded_message}")
             else:
-                printer.error("Invalid encoding, please choose either '64' or '32' or '16'..!")
-            printer.success(f"'{self.message}' in Base{self.encoding} : {self.encoded_message}")
+                printer.error("Invalid encoding, please choose either '64', '32', or '16'..!")
         except UnicodeEncodeError:
             printer.error("Invalid character, please only use ASCII characters.")
 
     def decode(self):
         try:
-            if self.encoding == "64":
-                self.decoded_message = base64.b64decode(self.message.encode("ascii")).decode("ascii")
-            elif self.encoding == "32":
-                self.decoded_message = base64.b32decode(self.message.encode("ascii")).decode("ascii")
-            elif self.encoding == "16":
-                self.decoded_message = base64.b16decode(self.message.encode("ascii")).decode("ascii")
-            printer.success(f"'{self.message}' in plain text : {self.decoded_message}")
+            if self.encoding in ("64", "32", "16"):
+                decoding_method = getattr(base64, f'b{self.encoding}decode')
+                self.decoded_message = decoding_method(self.message.encode("ascii")).decode("ascii")
+                printer.info(f"'{self.message}' in plain text : {self.decoded_message}")
+            else:
+                printer.error("Invalid encoding, please choose either '64', '32', or '16'..!")
         except Exception:
             printer.error("Error while decoding, please make sure the message is encoded in Base64.")
+
